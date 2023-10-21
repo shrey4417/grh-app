cb5099d6520bfbcd7865a3c9c21af4724117b27a
Shreyajit Saha <shreyansh4417@gmail.com>
2023-10-21 15:17:35 +0530

    grh hospital UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    NavComponent,
+    FooterComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
