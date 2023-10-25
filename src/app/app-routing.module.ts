2685fadd5c1b1254b38392b4fd816fba72adcf68
shrey4417 <shreyansh4417@gmail.com>
2023-10-25 08:52:20 +0530

    Added routing for nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FindMyDoctorComponent } from './components/find-my-doctor/find-my-doctor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'find-my-doctor', component:FindMyDoctorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { MainComponent } from './components/main/main.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AppointmentComponent } from './components/appointment/appointment.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DoctorPortalComponent } from './components/doctor-portal/doctor-portal.component';
 import { FindMyDoctorComponent } from './components/find-my-doctor/find-my-doctor.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent,
     NavComponent,
     FooterComponent,
     AppointmentComponent,
     DoctorPortalComponent,
-    FindMyDoctorComponent
+    FindMyDoctorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
